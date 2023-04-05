c65186bf2d991d8ab5e29fb73ba67d7619635459
brunoSouza07 <bruno_henrique2001@hotmail.com>
2023-04-05 12:03:32 -0300

    uploading right code

--- index.java
+++ index.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
-        
+        Scanner ler = new Scanner(System.in);
+        double num;
+        do {
+            System.out.print("Digite um número positivo: ");
+            while (!ler.hasNextDouble()) {//funciona enquanto não houver um double válido
+                System.out.println("Entrada inválida! Digite novamente: ");
+                ler.next(); //next pois tem que descartar x entrada anterior
+            }
+            num = ler.nextDouble();
+        } while (num <= 0);
+        ler.close();
     }
 }
